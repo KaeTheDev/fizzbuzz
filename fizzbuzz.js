cff6bdedd5e3d8b170c3c92ab9d931b5d92802e5
KaeTheDev <kaethedev@gmail.com>
2023-05-09 15:36:51 -0400

    Update to print instead of just adding to an array

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -4,12 +4,20 @@ function fizzbuzz() {
     for(var i = 1; i <= 100; i++) {
         if(i%3==0 && i%5==0) {
         arr.push("FizzBuzz")
+        console.log("FizzBuzz")
         }
         else if(i%5==0){
             arr.push("Buzz")
+            console.log("Buzz")
         }
         else if(i%3==0){
             arr.push("Fizz")
+            console.log("Fizz")
+        }
+
+        else {
+            arr.push(i);
+            console.log(i)
         }
     }
     return arr;
