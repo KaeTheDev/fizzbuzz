69daf215f0d1a7f59c653e3e1df03f50f04bee80
KaeTheDev <kaethedev@gmail.com>
2023-05-09 12:28:57 -0400

    First Commit

---
+++ fizzbuzz.js
@@ -0,0 +1,19 @@
+function fizzbuzz() {
+    var arr =[];
+
+    for(var i = 1; i <= 100; i++) {
+        if(i%3==0 && i%5==0) {
+        arr.push("FizzBuzz")
+        }
+        else if(i%5==0){
+            arr.push("Buzz")
+        }
+        else if(i%3==0){
+            arr.push("Fizz")
+        }
+    }
+    return arr;
+}
+
+var z = fizzbuzz();
+console.log(z);
